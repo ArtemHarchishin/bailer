30381bf09b896109b263cca02f523351ac1c09dc
Артем Харчишин <artem.kharchishin@ilogos.biz>
2019-07-23 23:16:54 +0300

    update

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask, jsonify
+import time
+import collections
 
 app = Flask(__name__)
 
+db = ([], {})
+
+FlowerEntry = collections.namedtuple('FlowerEntry', 'id, name, watering_interval, last_watering')
+
+def create_flower_entry(name, watering_interval):
+    flower_id = time.time()
+    return FlowerEntry(flower_id, name, watering_interval, flower_id)
+
+
 def add_flower(name, watering_interval):
-    return (name, watering_interval)
+    entry = create_flower_entry(name, watering_interval)
+
+    db[0].append(entry)
+    db[1][entry.id] = entry
+
 
 def delete_flower(name):
     pass
@@ -12,7 +27,12 @@ def watering_flower(name):
     pass
 
 def what_need_watering():
-    pass
+    res = []
+    for entry in db[0]:
+        next_watering = entry.last_watering + entry.watering_interval
+        if next_watering < time.time():
+            res.append(entry)
+    return res
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test_bailer.py
+++ test_bailer.py
@@ -1,7 +1,21 @@
 import app
 
 def test_add_flower():
-    pass
+    # Given
+    flower_name = "foo"
+    watering_interval = 1000
+    
+    # When 
+    app.add_flower(flower_name, watering_interval)
+
+    # Then
+    flowers = app.what_need_watering()
+    flower = flowers[0]
+    print(flower)
+    assert isinstance(flowers, list)
+    assert len(flowers) == 1
+    assert flower.name == flower_name
+    assert flower.watering_interval == watering_interval
 
 def test_remove_flower():
     pass
